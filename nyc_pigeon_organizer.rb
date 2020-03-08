a87b4d68dcfd3ba474d08ce0b4bd9643018c5f74
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-08 19:32:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,9 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # pigeon names are the keys, with each name pointing to a hash of attributes
+  
   newHash = {}
-  data.each do |property, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if !newHash.has_key?(name)
-          newHash[name] = {}
-        end
-
-        if !newHash[name].has_key?(property)
-          newHash[name][property] = []
-        end
-
-        if !newHash[name][property].include?(attribute)
-          newHash[name][property] << attribute.to_s
-        end
-      end
-    end
-  end
+  index = 0 
+#use block methods like map or reduce to find pigeon names and 
+#add them to a new hash  to pass the first two tests
   newHash
-
 end
